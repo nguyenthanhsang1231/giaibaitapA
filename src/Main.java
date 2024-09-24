0a1b2d1bf95cf3788298025e507d78c67019769b
httcfe <thanhsangdeptrai963@gmail.com>
2024-09-24 22:13:39 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //b1
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so dien: ");
+        int soDien = sc.nextInt();
+        int[] donGia = {1806, 1866, 2167, 2729, 3050, 3151};
+        int[] sanLuong = {50, 50, 100, 100, 100, 0};
+        int tongTien = 0;
+        for (int i = 0; i < donGia.length; i++) {
+            if (soDien > 0) {
+                int suDung;
+                if (soDien >= sanLuong[i]) {
+                    suDung = sanLuong[i];
+                } else {
+                    suDung = soDien;
+                }
+                tongTien += suDung * donGia[i];
+                soDien -= suDung;
+            }
+        }
+
+        System.out.println("Tong tien: " + tongTien + " dong");
+
+
+        //b2
+//        int[] menhGia = {100, 20, 5, 1};
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("nhap so tien can rut: ");
+//        int T = sc.nextInt();
+//        int[] soLuongTo = new int[menhGia.length];
+//        for (int i = 0; i < menhGia.length; i++) {
+//            soLuongTo[i] = T / menhGia[i];
+//            T %= menhGia[i];
+//        }
+//        for (int i = 0; i < menhGia.length; i++) {
+//            System.out.println("menh gia " + menhGia[i] + ": " + soLuongTo[i] + " vnd");
+//        }
+
+
+        // b3
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+
+    }
+}
