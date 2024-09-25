2ae942e75bb3c9b1997a504600c8db9c24372387
httcfe <thanhsangdeptrai963@gmail.com>
2024-09-25 08:40:22 +0700

    b3

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //b1
-        Scanner sc = new Scanner(System.in);
-        System.out.println("nhap so dien: ");
-        int soDien = sc.nextInt();
-        int[] donGia = {1806, 1866, 2167, 2729, 3050, 3151};
-        int[] sanLuong = {50, 50, 100, 100, 100, 0};
-        int tongTien = 0;
-        for (int i = 0; i < donGia.length; i++) {
-            if (soDien > 0) {
-                int suDung;
-                if (soDien >= sanLuong[i]) {
-                    suDung = sanLuong[i];
-                } else {
-                    suDung = soDien;
-                }
-                tongTien += suDung * donGia[i];
-                soDien -= suDung;
-            }
-        }
-
-        System.out.println("Tong tien: " + tongTien + " dong");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("nhap so dien: ");
+//        int soDien = sc.nextInt();
+//        int[] donGia = {1806, 1866, 2167, 2729, 3050, 3151};
+//        int[] sanLuong = {50, 50, 100, 100, 100, 0};
+//        int tongTien = 0;
+//        for (int i = 0; i < donGia.length; i++) {
+//            if (soDien > 0) {
+//                int suDung;
+//                if (soDien >= sanLuong[i]) {
+//                    suDung = sanLuong[i];
+//                } else {
+//                    suDung = soDien;
+//                }
+//                tongTien += suDung * donGia[i];
+//                soDien -= suDung;
+//            }
+//        }
+//
+//        System.out.println("Tong tien: " + tongTien + " dong");
 
 
         //b2
@@ -41,8 +43,22 @@ public class Main {
 
 
         // b3
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int start = sc.nextInt();
 
+        List<List<Test.E>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            graph.get(u).add(new Test.E(v, w));
+        }
+        Test.dijkstra(n, start, graph);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,52 @@
-public class Test {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 
+public class Test {
+    static class E {
+        int to, weight;
+        E(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    public static void dijkstra(int n, int start, List<List<E>> graph) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparing(a -> a[1]));
+        pq.add(new int[]{start, 0});
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int u = cur[0];
+            int current = cur[1];
+
+            if (current > dist[u]) {
+                continue;
+            }
+
+            for (E e : graph.get(u)) {
+                int v = e.to;
+                int w = e.weight;
+
+                if(dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.add(new int[] {v, dist[v]});
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] != Integer.MAX_VALUE) {
+                System.out.println(-1);
+            } else {
+                System.out.println(dist[i]);
+            }
+        }
+    }
 }
